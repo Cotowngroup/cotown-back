274cee404e72dabf2e905fe26c1adbf0f20a9a0f
alejandro-andre <alejandro.andre@oimbra.tech>
2026-09-08 12:39:20 +0200

    Discard without documentation on time

--- function/Booking.Booking_CRON_workflow.sql
+++ function/Booking.Booking_CRON_workflow.sql
@@ -20,19 +20,11 @@ BEGIN
   -- Descarta las solicitudes confirmadas sin documentación pasadas de fecha
   BEGIN
     UPDATE "Booking"."Booking"
-    SET "Status"='descartada'
+    SET "Status"='documentacionko'
     WHERE "Booking"."Documentation_limit" < CURRENT_DATE
-    AND "Booking"."Status" = 'pendientepago';
+    AND "Booking"."Status" IN ('pendientepago', 'confirmada');
   EXCEPTION WHEN OTHERS THEN
-    RAISE NOTICE 'Error caducando solicitudes sin documentacion pasadas de fecha: % %', SQLSTATE, SQLERRM;
-  END; 
-  BEGIN
-    UPDATE "Booking"."Booking"
-    SET "Status"='descartadapagada'
-    WHERE "Booking"."Documentation_limit" < CURRENT_DATE
-    AND "Booking"."Status" = 'confirmada';
-  EXCEPTION WHEN OTHERS THEN
-    RAISE NOTICE 'Error caducando solicitudes pagadas sin documentacion pasadas de fecha: % %', SQLSTATE, SQLERRM;
+    RAISE NOTICE 'Error descartando solicitudes sin documentacion pasadas de fecha: % %', SQLSTATE, SQLERRM;
   END; 
 
   -- Actualiza el estado a 'descartada' de todas las solicitudes que esten caducadas con la fecha de caducidad a NULL o
